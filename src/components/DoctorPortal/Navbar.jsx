71c0ee627ad39366c70822fced4be8c0cf10242d
Hammad <Hammad7106>
2025-01-22 22:50:06 +0500

    Updations in Doctor's Page

--- src/components/DoctorPortal/Navbar.jsx
+++ src/components/DoctorPortal/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { Routes, Route, Link as RouterLink } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Link as RouterLink, useNavigate } from "react-router-dom";
 import { Link } from "react-scroll";
-import Button from '../DoctorPortal/layouts/Button';
+import Button from "../DoctorPortal/layouts/Button";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import Contact from "../DoctorPortal/models/Contact";
 import MainNavbar from "../MainPage/MainNavbar";
@@ -9,6 +9,10 @@ import MainNavbar from "../MainPage/MainNavbar";
 const Navbar = () => {
   const [menu, setMenu] = useState(false);
   const [showForm, setShowForm] = useState(false);
+  const navigate = useNavigate();
+  const firstName = localStorage.getItem("first_name");
+  const userId = localStorage.getItem("user_id");
+
 
   const handleChange = () => {
     setMenu(!menu);
@@ -26,14 +30,18 @@ const Navbar = () => {
   const closeForm = () => {
     setShowForm(false);
   };
-
+  const handleFirstNameClick = () => {
+    if (userId) {
+      navigate(`/doctor_profile/${userId}`);
+    }
+  };
   return (
     <div className="fixed w-full z-10 text-white">
       <div>
         <div className="flex flex-row justify-between p-6 md:px-32 px-5 bg-backgroundColor shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
           <div className="flex flex-row items-center cursor-pointer">
             <Link to="home" spy={true} smooth={true} duration={500}>
-              <h1 className="text-2xl font-semibold">Diabeat.</h1>
+              <h1 className="text-2xl font-semibold" onClick={()=> navigate('/')}>Diabeat</h1>
             </Link>
           </div>
 
@@ -53,15 +61,6 @@ const Navbar = () => {
             >
               About Us
             </Link>
-            {/* <Link
-              to="services"
-              spy={true}
-              smooth={true}
-              duration={500}
-              className="hover:text-yellow-300 transition-all cursor-pointer"
-            >
-              Services
-            </Link> */}
             <Link
               to="doctors"
               spy={true}
@@ -69,7 +68,7 @@ const Navbar = () => {
               duration={500}
               className="hover:text-yellow-300 transition-all cursor-pointer"
             >
-              Doctors
+              Messages
             </Link>
             <Link
               to="blog"
@@ -85,9 +84,9 @@ const Navbar = () => {
           <div className="hidden lg:flex">
             <button
               className="bg-brightColor text-white px-4 py-2 rounded-md hover:bg-yellow-300 transition duration-300 ease-in-out"
-              onClick={openForm}
+              onClick={handleFirstNameClick}
             >
-              Contact Us
+              {firstName ? `Doctor ${firstName}` : "Contact Us"}
             </button>
           </div>
           {showForm && <Contact closeForm={closeForm} />}
@@ -100,6 +99,7 @@ const Navbar = () => {
             )}
           </div>
         </div>
+
         <div
           className={`${menu ? "translate-x-0" : "-translate-x-full"
             } lg:hidden flex flex-col absolute bg-backgroundColor text-white left-0 top-16 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300`}
@@ -121,16 +121,6 @@ const Navbar = () => {
           >
             About Us
           </Link>
-          {/* <Link
-            to="services"
-            spy={true}
-            smooth={true}
-            duration={500}
-            className="hover:text-hoverColor transition-all cursor-pointer"
-            onClick={closeMenu}
-          >
-            Services
-          </Link> */}
           <Link
             to="doctors"
             spy={true}
@@ -139,7 +129,7 @@ const Navbar = () => {
             className="hover:text-hoverColor transition-all cursor-pointer"
             onClick={closeMenu}
           >
-            Doctors
+            Messages
           </Link>
           <Link
             to="blog"
@@ -155,9 +145,9 @@ const Navbar = () => {
           <div className="lg:hidden">
             <button
               className="bg-brightColor text-white px-4 py-2 rounded-md hover:bg-hoverColor transition duration-300 ease-in-out"
-              onClick={openForm}
+              onClick={handleFirstNameClick}
             >
-              Contact Us
+              {firstName ? `Doctor ${firstName}` : "Contact Us"}
             </button>
           </div>
         </div>
